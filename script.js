ddae191a70a5351606fa9fdb0cbde6911331da17
Sagar Hande <sagarhande1947@gmail.com>
2022-07-03 18:13:24 +0530

    added song play/pause and gif opacity feature using JS

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
 console.log("Welcome to Spotify")
+// Initialize variables
+let songIndex = 0
+let audioElement = new Audio("/songs/1.mp3")
+let masterPlay = document.getElementById("masterPlay")
+let myProgressBar = document.getElementById("myProgressBar")
+let gif = document.getElementById("gif")
+
+
 let songs= [
     {songName:"Song1", filePath: "songs/1.mp3", coverPath: "images/covers/1.jpg"},
     {songName:"Song1", filePath: "songs/1.mp3", coverPath: "images/covers/1.jpg"},
@@ -8,4 +16,22 @@ let songs= [
     {songName:"Song1", filePath: "songs/1.mp3", coverPath: "images/covers/1.jpg"},
     {songName:"Song1", filePath: "songs/1.mp3", coverPath: "images/covers/1.jpg"},
 ]
-let audioElement = new Audio("/songs/1.mp3")
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0) {
+        audioElement.play()
+        masterPlay.classList.remove('fa-play-circle')
+        masterPlay.classList.add('fa-pause-circle')
+        gif.style.opacity = 1
+    }else {
+        audioElement.pause()
+        masterPlay.classList.remove('fa-pause-circle')
+        masterPlay.classList.add('fa-play-circle')
+        gif.style.opacity = 0
+    }
+})
+// Listen to events
+myProgressBar.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate')
+})
