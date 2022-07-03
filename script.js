9ab666b9acfe23a31ae702a026bde01973cab132
Sagar Hande <sagarhande1947@gmail.com>
2022-07-03 17:29:27 +0530

    added playing gif and background image

---
+++ script.js
@@ -0,0 +1,11 @@
+
+console.log("Welcome to Spotify")
+let songs= [
+    {songName:"Song1", filePath: "songs/1.mp3", coverPath: "images/covers/1.jpg"},
+    {songName:"Song1", filePath: "songs/1.mp3", coverPath: "images/covers/1.jpg"},
+    {songName:"Song1", filePath: "songs/1.mp3", coverPath: "images/covers/1.jpg"},
+    {songName:"Song1", filePath: "songs/1.mp3", coverPath: "images/covers/1.jpg"},
+    {songName:"Song1", filePath: "songs/1.mp3", coverPath: "images/covers/1.jpg"},
+    {songName:"Song1", filePath: "songs/1.mp3", coverPath: "images/covers/1.jpg"},
+]
+let audioElement = new Audio("/songs/1.mp3")
